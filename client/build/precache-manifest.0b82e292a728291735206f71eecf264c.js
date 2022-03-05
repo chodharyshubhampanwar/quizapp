7829587aa4d5a08e77f8378e3a7cb50d2404c024
Shubham Panwar <shubhamcpanwar@outlook.com>
2022-03-05 17:57:08 +0530

    deploying to heroku

---
+++ client/build/precache-manifest.0b82e292a728291735206f71eecf264c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339b6aa76a4683b75e2be2fe0d0800ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96908216982d2b486330",
+    "url": "/static/css/main.b85718ca.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96908216982d2b486330",
+    "url": "/static/js/main.26a56c9b.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
